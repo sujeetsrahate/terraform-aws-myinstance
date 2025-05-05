ffe561421eea577ea4f312ed10a6052ba86cfef3
Sujeet Rahate <sujeetsrahate@gmail.com>
2025-05-05 15:55:44 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "aws_ami_id" {
+    description = "The AMI ID to use for the instance"
+    type = string
+    }
+
+variable "instance_type" {
+    description = "The type of the instance"
+    type = string
+}
